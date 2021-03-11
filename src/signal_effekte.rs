37c3c1094bd6c67d0ae86c1b407100521d891bd8
TheArtistGuy <info@kaimueller-art.de>
2021-03-11 15:22:38 +0100

    bugfix in echo

--- src/signal_effekte.rs
+++ src/signal_effekte.rs
@@ -1,12 +1,12 @@
 pub fn echo (signal : Vec<f32>, verzoegerung : usize, amplitude : f32) ->  Vec<f32>{
 
-    let mut output = Vec::new();
+    let mut output : Vec<f32> = Vec::new();
     for (index, abtastpunkt) in signal.iter().enumerate(){
         let punkt = (index, abtastpunkt);
-        let ausgabepunkt = match punkt {
-            ( 0..verzoegerung, x) => x.clone(),
-            (i, x)  =>
-                x+ amplitude * signal.get(i - verzoegerung).unwrap(),
+        let ausgabepunkt  = if index <= verzoegerung {
+            abtastpunkt.clone()
+        } else {
+                abtastpunkt + amplitude * signal.get(index - verzoegerung).unwrap().clone()
         };
         output.push(ausgabepunkt);
     }
