a120b607a67b2f30489148d23a3478cbc1dff430
TheArtistGuy <info@kaimueller-art.de>
2021-04-24 20:20:18 +0200

    falschen Kommentar gelöscht

--- src/bildbearbeitungs_kernels.rs
+++ src/bildbearbeitungs_kernels.rs
@@ -1,16 +1,4 @@
-/// 3x3 Kernel für gaussches Weichzeichnen
 
-fn gauss_3x3 (input : &[[u8; 3]; 3]) -> f32{
-    let mut sum  = 0.0;
-    let matrix_3x3 = Faltungskern3x3::gauss();
-    for x in 0..2 {
-        for y in 0..2{
-            sum = sum + f32::from(input[x][y])  * matrix_3x3.matrix[x][y];
-        }
-    }
-    sum
-
-}
 
 
 
