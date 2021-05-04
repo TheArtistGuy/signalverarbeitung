893a6d567ea6ae8170113be139046039ba85760b
TheArtistGuy <info@kaimueller-art.de>
2021-05-04 20:01:44 +0200

    DoG hinzugefügt

--- src/merkmaldetektion.rs
+++ src/merkmaldetektion.rs
@@ -1,8 +1,20 @@
 use crate::bild_intensitaet::BildIntenstiaet;
+use crate::bildbearbeitungs_kernels::gaussche_unschaerfe;
 
 pub fn SIFT (bild : BildIntenstiaet) -> Box<[f32]> {
     //TODO implementieren
-    x
+    Box::from([0.0])
 }
 
+pub fn difference_of_gaussian(bild : BildIntenstiaet) -> BildIntenstiaet{
+    let gauss = gaussche_unschaerfe(&bild);
+    let mut dog = BildIntenstiaet::new(bild.hoehe, bild.breite);
+    for i in 0..bild.hoehe{
+        for j in 0..bild.breite{
+            dog.set_pixel(i,j,
+                bild.get_pixel(i,j).unwrap() - dog.get_pixel(i,j).unwrap());
+        }
+    }
+    dog
+}
 
